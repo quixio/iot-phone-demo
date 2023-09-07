eb44e63204b3c4e8aa035ec7e6db7f7ad74770b0
Tomas Neubauer <tomas@quix.io>
2023-09-07 11:28:38 +0000

    Edited 'main.py'

--- ShakeDetection/main.py
+++ ShakeDetection/main.py
@@ -2,29 +2,13 @@ import quixstreams as qx
 from sdk.stream_reader_new import StreamReaderNew
 from sdk.stream_writer_new import StreamWriterNew
 import os
-from azure.storage.blob import BlobClient
-import pickle
-
-
-blob = BlobClient.from_connection_string(
-    "DefaultEndpointsProtocol=https;AccountName=quixmodelregistry;AccountKey=9OkHZOhAW+1vtwWjReLKLQ8zyPzB0lDjaxjpTvIxaCrrlfe5rBehIc2NexmrrlyZoyUokfxlBkuaLUVUpoUoBQ==;EndpointSuffix=core.windows.net",
-    "models",
-    "XGB_model.pkl")
-
-with open("XGB_model.pkl", "wb+") as my_blob:
-    blob_data = blob.download_blob()
-    blob_data.readinto(my_blob)
-loaded_model = pickle.load(open("XGB_model.pkl", 'rb'))
-
-def predict(row):
-    return loaded_model.predict(row[["gForceZ","gForceY","gForceX","gForceTotal"]].to_pandas_series())[0]
 
 client = qx.QuixStreamingClient()
 
 print("Opening input and output topics")
 
-input_topic = client.get_topic_consumer("phone-data", "v3.4", auto_offset_reset=qx.AutoOffsetReset.Latest)
-output_topic = client.get_topic_producer("events")
+input_topic = client.get_topic_consumer(os.environ["input"], "v3.4", auto_offset_reset=qx.AutoOffsetReset.Latest)
+output_topic = client.get_topic_producer(os.environ["output"])
 
 async def on_new_stream(input_stream: StreamReaderNew, output_stream: StreamWriterNew):
     print("New stream: " + input_stream.stream_id)
@@ -35,9 +19,7 @@ async def on_new_stream(input_stream: StreamReaderNew, output_stream: StreamWrit
 
     df["gForceTotal"] = df.apply(lambda x: abs(x["gForceX"]) +  abs(x["gForceY"]) +  abs(x["gForceZ"]) )
 
-    df["gForceTotal_10s"] = df["gForceTotal"].rolling("10s").mean()
-
-    df["shaking"] = df.apply(predict)
+    df["shaking"] = df.apply(lambda x: 1 if x["gForceTotal"] > 15 else 0)
 
     df.set_columns_print_width(10)
 

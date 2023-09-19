0a0c27d17c234927d2d7a8318af84ac539c39c0d
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2023-09-19 18:45:45 +0200

    Migration to SDK 1.0 (#9)
    * Edited 'main.py'
    
    ---------
    
    Co-authored-by: Tomas Neubauer <tomas@quix.io>

--- ShakeDetection/main.py
+++ ShakeDetection/main.py
@@ -2,6 +2,7 @@ import quixstreams as qx
 from sdk.stream_reader_new import StreamReaderNew
 from sdk.stream_writer_new import StreamWriterNew
 import os
+import pandas as pd
 
 client = qx.QuixStreamingClient()
 
@@ -10,28 +11,34 @@ print("Opening input and output topics")
 input_topic = client.get_topic_consumer(os.environ["input"], "v3.4", auto_offset_reset=qx.AutoOffsetReset.Latest)
 output_topic = client.get_topic_producer(os.environ["output"])
 
-async def on_new_stream(input_stream: StreamReaderNew, output_stream: StreamWriterNew):
-    print("New stream: " + input_stream.stream_id)
 
-    df = input_stream.df[["gForceX", "gForceY", "gForceZ"]]
+def on_dataframe_received(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    
+    if "gForceX" in df: 
+        df["gForceTotal"] = df["gForceX"].abs() + df["gForceY"].abs() + df["gForceZ"].abs()
+        df["shaking"] = df["gForceTotal"].apply(lambda x: 1 if x > 15 else 0)
 
-    df = df[df["gForceX"] != None]
+        print(df[["gForceTotal", "shaking"]])
 
-    df["gForceTotal"] = df.apply(lambda x: abs(x["gForceX"]) +  abs(x["gForceY"]) +  abs(x["gForceZ"]) )
-
-    df["shaking"] = df.apply(lambda x: 1 if x["gForceTotal"] > 15 else 0)
-
-    df.set_columns_print_width(10)
-
-    print(df.header)
-
-    async for row in df:
-        print(row)
-        await output_stream.write(row)
+        output_topic.get_or_create_stream(stream_consumer.stream_id).timeseries.publish(df)
 
 
 
-StreamReaderNew.process_stream(input_topic, output_topic, on_new_stream)
+def on_stream_received(stream_consumer: qx.StreamConsumer):
+    print("New stream: " + stream_consumer.stream_id)
+
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received
+
+
+
+
+ 
+
+  
+
+
+input_topic.on_stream_received = on_stream_received
+
 
 print("Listening to streams. Press CTRL-C to exit.")
 qx.App.run()

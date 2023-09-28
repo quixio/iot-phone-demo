d08593b6a6a828ee4ebc056cb5e94d0bdf7d9404
Tomas x <tomas+demo@quix.io>
2023-09-28 20:33:25 +0000

    Edited 'main.py'

--- Splitter/main.py
+++ Splitter/main.py
@@ -6,31 +6,19 @@ import pandas as pd
 client = qx.QuixStreamingClient()
 
 topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
-gps_topic_producer = client.get_topic_producer(os.environ["gps"])
-gps_topic_producer = client.get_topic_producer(os.environ["gps"])
+gps_topic_producer = client.get_topic_producer(os.environ["gps_topic"])
+gforce_topic_producer = client.get_topic_producer(os.environ["gforce_topic"])
 
 
 def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    
+    topic_producer = gforce_topic_producer if "gForceX" in df else gps_topic_producer 
 
-    # Transform data frame here in this method. You can filter data or add new features.
-    # Pass modified data frame to output stream using stream producer.
-    # Set the output stream id to the same as the input stream or change it,
-    # if you grouped or merged data with different key.
     stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
     stream_producer.timeseries.buffer.publish(df)
 
 
-# Handle event data from samples that emit event data
-def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
-    print(data)
-    # handle your event data here
-
-
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    # subscribe to new DataFrames being received
-    # if you aren't familiar with DataFrames there are other callbacks available
-    # refer to the docs here: https://docs.quix.io/sdk/subscribe.html
-    stream_consumer.events.on_data_received = on_event_data_received_handler # register the event data callback
     stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
 
 

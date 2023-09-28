bda6e303bb432f36c6280441e03f01d49d016bc6
Tomas x <tomas+demo@quix.io>
2023-09-28 21:25:28 +0000

    Edited 'main.py'

--- ModelTraining/main.py
+++ ModelTraining/main.py
@@ -1,47 +1,7 @@
-# Importing necessary libraries and modules
-import quixstreams as qx
 import os
 from threading import Thread
 import influxdb_client_3 as InfluxDBClient3
 from time import sleep
-
-# Helper function to convert time intervals (like 1h, 2m) into seconds for easier processing.
-# This function is useful for determining the frequency of certain operations.
-def interval_to_seconds(interval):
-    if not interval:
-        raise ValueError("Invalid interval string")
-
-    unit = interval[-1].lower()
-    try:
-        value = int(interval[:-1])
-    except ValueError:
-        raise ValueError("Invalid interval format")
-
-    if unit == 's':
-        return value
-    elif unit == 'm':
-        return value * 60
-    elif unit == 'h':
-        return value * 3600
-    elif unit == 'd':
-        return value * 3600 * 24
-    elif unit == 'mo':
-        return value * 3600 * 24 * 30
-    elif unit == 'y':
-        return value * 3600 * 24 * 365
-    else:
-        raise ValueError(f"Unknown interval unit: {unit}")
-
-
-# should the main loop run?
-# Global variable to control the main loop's execution
-run = True
-
-# Quix provides automatic credential injection for the client.
-# However, if needed, the SDK token can be provided manually.
-# Alternatively, you can always pass an SDK token manually as an argument.
-client = qx.QuixStreamingClient()
-
 
 client = InfluxDBClient3.InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
                          host=os.environ["INFLUXDB_HOST"],
@@ -53,7 +13,7 @@ measurement_name = os.environ.get("INFLUXDB_MEASUREMENT_NAME")
 
 try:
     # Query InfluxDB 3.0 usinfg influxql or sql
-    table = client.query(query=f"SELECT * FROM {measurement_name} WHERE time >= now() - {interval}", language="influxql")
+    table = client.query(query=f"SELECT * FROM \"gforce\" WHERE time >= now() - interval '1 hour'", language="influxql")
 
     # Convert the result to a pandas dataframe. Required to be processed through Quix. 
     df = table.to_pandas().drop(columns=["iox::measurement"])

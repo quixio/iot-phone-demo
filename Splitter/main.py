ef504032b189922b4bd81f6fa8d44931906d35bc
Tomas x <tomas+demo@quix.io>
2023-09-28 20:35:40 +0000

    Edited 'main.py'

--- Splitter/main.py
+++ Splitter/main.py
@@ -1,6 +1,7 @@
 import quixstreams as qx
 import os
 import pandas as pd
+from tqdm import tqdm
 
 
 client = qx.QuixStreamingClient()
@@ -17,6 +18,8 @@ def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.Dat
     stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
     stream_producer.timeseries.buffer.publish(df)
 
+    pbar.update(df.shape[0])
+
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
@@ -26,6 +29,8 @@ def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
 topic_consumer.on_stream_received = on_stream_received_handler
 
 print("Listening to streams. Press CTRL-C to exit.")
+
+pbar = tqdm(unit="item", leave=True)
 
 # Handle termination signals and provide a graceful exit
 qx.App.run()

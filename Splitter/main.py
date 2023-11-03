c906497b0e0c7df863c07438b12638bfc59d51a7
Tomas x <tomas+demo@quix.io>
2023-11-03 15:42:06 +0000

    Edited 'main.py'

--- Splitter/main.py
+++ Splitter/main.py
@@ -1,40 +1,91 @@
-import quixstreams as qx
+
+#from quix_function import QuixFunction
 import os
+from streamingdataframes import Application, MessageContext, State
+from streamingdataframes.models.rows import Row
+from streamingdataframes.models.serializers import (
+    QuixTimeseriesSerializer,
+    QuixDeserializer,
+    JSONDeserializer
+)
+import signal
+from azure.storage.blob import BlobClient
+import pickle
 import pandas as pd
-from tqdm import tqdm
+
+model = os.environ["model"]
+
+blob = BlobClient.from_connection_string(
+    "DefaultEndpointsProtocol=https;AccountName=quixmodelregistry;AccountKey=9OkHZOhAW+1vtwWjReLKLQ8zyPzB0lDjaxjpTvIxaCrrlfe5rBehIc2NexmrrlyZoyUokfxlBkuaLUVUpoUoBQ==;EndpointSuffix=core.windows.net",
+    "models",
+    model)
+
+with open(model, "wb+") as my_blob:
+    blob_data = blob.download_blob()
+    blob_data.readinto(my_blob)
+
+print("Loaded")
+
+loaded_model = pickle.load(open(model, 'rb'))
+
+# Quix app has an option to auto create topics
+# Quix app does not require the broker being defined
+app = Application.Quix("big-query-sink-v3", auto_offset_reset="earliest", )
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 
-client = qx.QuixStreamingClient()
 
-topic_consumer = client.get_topic_consumer(
-    os.environ["input"], 
-    consumer_group = "splitter",
-    auto_offset_reset=qx.AutoOffsetReset.Earliest)
-
-gps_topic_producer = client.get_topic_producer(os.environ["gps_topic"])
-gforce_topic_producer = client.get_topic_producer(os.environ["gforce_topic"])
+def print_row(row: Row, ctx: MessageContext):
+    print(row)
 
 
-def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-    
-    topic_producer = gforce_topic_producer if "gForceX" in df else gps_topic_producer 
-
-    stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-    stream_producer.timeseries.buffer.publish(df)
-
-    pbar.update(df.shape[0])
-
-
-def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
-
-
-# subscribe to new streams being received
-topic_consumer.on_stream_received = on_stream_received_handler
-
+# Hook up to termination signal (for docker image) and CTRL-C
 print("Listening to streams. Press CTRL-C to exit.")
 
-pbar = tqdm(unit="item", leave=True)
+def fill_gaps(value, ctx):
+    if 'gForceX' not in value:
+        value["gForceX"] = None
+        value["gForceY"] = None
+        value["gForceZ"] = None
 
-# Handle termination signals and provide a graceful exit
-qx.App.run()
+
+def rolling_window(value: dict, ctx: MessageContext, state: State):
+    
+    state_value = state.get("rolling_10s", {})
+    state_value = {int(k): v for k,v in state_value.items()}
+
+    state_value[value["Timestamp"]] = value["gForceTotal"]
+    timestamps = state_value.keys()
+    last_timestamp = max(timestamps)
+    filtered_window = {}
+    for key in state_value:
+        if (last_timestamp - key) < 10000000000:
+            filtered_window[key] = state_value[key]
+    
+    state.set("rolling_10s", filtered_window)
+    data_df = pd.DataFrame([{'gForceZ': value["gForceZ"], 'gForceY': value["gForceY"], 'gForceX': value["gForceX"], 'gForceTotal': value["gForceTotal"]}])
+    value["gForceTotal_10s"] = sum(filtered_window.values()) / len(filtered_window)
+    value["shaking"] = int(loaded_model.predict(data_df)[0])
+
+
+
+# "Gold" members get realtime notifications about purchase events larger than $1000
+sdf = app.dataframe(topics_in=[input_topic])
+sdf = sdf.apply(fill_gaps)
+sdf = sdf[sdf["gForceX"].isnot(None)]
+sdf = sdf[["Timestamp", "gForceX", "gForceY", "gForceZ"]]
+
+sdf["gForceTotal"] = sdf["gForceX"] + sdf["gForceY"] + sdf["gForceZ"]
+sdf.apply(rolling_window, stateful=True)
+sdf.apply(print_row)  # easy way to print out
+
+sdf.to_topic(output_topic)
+
+signal.signal(signal.SIGINT, lambda _,_2: print("SIGINT"))
+signal.signal(signal.SIGTERM, lambda _,_2: print("SIGTERM"))
+
+try:
+    app.run(sdf)
+finally:
+    print("Gracefully shutdowned.")

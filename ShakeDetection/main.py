1b6e47861615b9e5068f273dd840393606bcb0a6
Tomas x <tomas+demo@quix.io>
2023-11-06 14:40:22 +0000

    Edited 'main.py'

--- ShakeDetection/main.py
+++ ShakeDetection/main.py
@@ -33,7 +33,7 @@ def gForceTotalSum(row: dict, ctx, state: State):
 
 sdf = sdf.apply(gForceTotalSum, stateful=True)
 
-sdf = sdf[["gForceTotal", "sum"]]
+sdf = sdf[["gForceTotal", "sum", "shaking"]]
 sdf["shaking"] = sdf["shaking"].apply(lambda v, ctx: 1 if v else 0)
 
 sdf.apply(lambda row,ctx: print(row))  # easy way to print out

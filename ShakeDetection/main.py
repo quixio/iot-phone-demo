029eb165d927f5ef89e2edc8e6dcfc8344f065a5
Tomas x <tomas+demo@quix.io>
2023-11-07 09:30:47 +0000

    Edited 'main.py'

--- ShakeDetection/main.py
+++ ShakeDetection/main.py
@@ -13,7 +13,7 @@ sdf = app.dataframe(input_topic)
 sdf["gForceTotal"] = sdf["gForceX"].abs() + sdf["gForceY"].abs() + sdf["gForceZ"].abs()
 sdf["shaking"] = sdf["gForceTotal"] > 15
 sdf["shaking"] = sdf["shaking"].apply(lambda value, _: 1 if value else 0)
-sdf = sdf[["shaking", "gForceTotal"]]
+#sdf = sdf[["shaking", "gForceTotal"]]
 
 sdf.apply(lambda row, ctx: print(row))
 

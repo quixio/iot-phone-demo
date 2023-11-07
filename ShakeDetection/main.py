e1d90a895bf349338aae65d4752eec830148d1e3
Tomas x <tomas+demo@quix.io>
2023-11-07 17:25:43 +0000

    Edited 'main.py'

--- ShakeDetection/main.py
+++ ShakeDetection/main.py
@@ -12,21 +12,8 @@ sdf = app.dataframe(input_topic)
 
 sdf["gForceTotal"] = sdf["gForceX"].abs() + sdf["gForceY"].abs() + sdf["gForceZ"].abs()
 
-sdf["shaking"] = sdf["gForceTotal"] > 15
-sdf["shaking"] = sdf["shaking"].apply(lambda value, ctx: 1 if value else 0)
 
 
-def sum_gForceTotal(row: dict, ctx, state:State):
-    sum_value = state.get("sum", 0)
-    sum_value += row["gForceTotal"]
-
-    row["sum"] = sum_value
-
-    state.set("sum", sum_value)
-    
-
-sdf.apply(sum_gForceTotal, stateful=True)
-sdf[["Timestamp", "gForceTotal", "sum"]]
 
 sdf.apply(lambda row, ctx: print(row))
 

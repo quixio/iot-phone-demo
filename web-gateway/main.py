79dd22af6ec9dd381dcd89c1355c709c675910e8
Tomas Neubauer <tomas@quix.io>
2024-04-22 13:26:39 +0000

    Edited 'main.py'

--- web-gateway/main.py
+++ web-gateway/main.py
@@ -4,10 +4,9 @@ import json
 from flask import Flask, request, Response
 from waitress import serve
 
-# Ensure these imports are correctly set up in your project
 from setup_logging import get_logger
+
 from quixstreams import Application
-from flask_restx import Api, Resource
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
@@ -20,25 +19,23 @@ producer = quix_app.get_producer()
 logger = get_logger()
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='API Documentation', description='A simple API')
 
-ns = api.namespace('my_namespace', description='Namespace operations')
 
-@ns.route('/', methods=['POST'])
-class WebServer(Resource):
+@app.route("/data/", methods=['POST'])
+def post_data():
+    
+    data = request.json
 
-    def post(self):
-        data = request.json
-        logger.info(f"{str(datetime.datetime.utcnow())} posted: {data}")
-        
-        # It's recommended to catch potential exceptions here for robustness
-        try:
-            producer.produce(topic.name, json.dumps(data), "test data")
-        except Exception as e:
-            logger.error(f"Failed to produce to topic: {e}")
-            return {"error": "Internal server error"}, 500
-        
-        return {"message": "Data posted successfully"}, 200
+    print(data)
+
+    logger.info(f"{str(datetime.datetime.utcnow())} posted.")
+    
+    producer.produce(topic.name, json.dumps(data), data["sessionId"])
+
+    response = Response(status=200)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 if __name__ == '__main__':

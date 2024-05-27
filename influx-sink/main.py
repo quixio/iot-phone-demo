c6b123ad5e966885ecec8f2ee7cca368ff30ebc9
Tomas Neubauer <tomas@quix.io>
2024-05-27 13:43:24 +0000

    Edited 'main.py'

--- influx-sink/main.py
+++ influx-sink/main.py
@@ -17,7 +17,12 @@ from influxdb_client_3 import Point, InfluxDBClient3
 from dotenv import load_dotenv
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] - %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 logger = logging.getLogger(__name__)
 
 # read the consumer group from config

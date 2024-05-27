a61b1a9f53a859a8918e1de17b6f4a2ed217168c
Tomas Neubauer <tomas@quix.io>
2024-05-27 13:45:44 +0000

    Edited 'main.py'

--- influx-sink/main.py
+++ influx-sink/main.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='[%(asctime)s] [%(levelname)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )

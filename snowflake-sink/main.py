ffe5907122a5d2b9aff53b904644de906ee3dc02
Tomas Neubauer <tomas@quix.io>
2024-12-04 13:19:32 +0000

    aaa

--- snowflake-sink/main.py
+++ snowflake-sink/main.py
@@ -1,1 +1,15 @@
-dasdasda
+import os
+from sink import SnowflakeSink
+
+# Load environment variables
+SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
+SNOWFLAKE_USER = os.getenv('SNOWFLAKE_USER')
+SNOWFLAKE_PASSWORD = os.getenv('SNOWFLAKE_PASSWORD')
+SNOWFLAKE_DATABASE = os.getenv('SNOWFLAKE_DATABASE')
+SNOWFLAKE_SCHEMA = os.getenv('SNOWFLAKE_SCHEMA')
+
+# Create Snowflake sink
+sink = SnowflakeSink(SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD, SNOWFLAKE_DATABASE, SNOWFLAKE_SCHEMA)
+
+# Use the sink
+sink.write(data)

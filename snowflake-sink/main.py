0505b48b9e2c8875636afd7708f3a49fe242deff
Tomas Neubauer <tomas@quix.io>
2024-12-05 14:04:40 +0000

    Edited 'main.py'

--- snowflake-sink/main.py
+++ snowflake-sink/main.py
@@ -1,22 +1,39 @@
 import os
+from setup_logger import logger
+
 from quixstreams import Application
-from dotenv import load_dotenv
+
+# for local dev, load env vars from a .env file
+#from dotenv import load_dotenv
+#load_dotenv()
+
 from sink import SnowflakeSink
 
-load_dotenv()
-
 TABLE_NAME = os.environ["TABLE_NAME"]
-USER = os.environ["USER"]
-PASSWORD = os.environ["PASSWORD"]
-ACCOUNT = os.environ["ACCOUNT"]
 WAREHOUSE = os.environ["WAREHOUSE"]
 DATABASE = os.environ["DATABASE"]
 SCHEMA = os.environ["SCHEMA"]
+ACCOUNT = os.environ["ACCOUNT"]
+USER = os.environ["USER"]
+PASSWORD = os.environ["PASSWORD"]
 
-snowflake_sink = SnowflakeSink(USER, PASSWORD, ACCOUNT, WAREHOUSE, DATABASE, SCHEMA, TABLE_NAME)
+snowflake_sink = SnowflakeSink(
+    WAREHOUSE, 
+    DATABASE, 
+    SCHEMA,
+    TABLE_NAME, 
+    ACCOUNT,
+    USER,
+    PASSWORD,
+    logger)
+
 snowflake_sink.connect()
 
-app = Application(consumer_group=os.environ["CONSUMER_GROUP"], auto_offset_reset = "earliest", commit_interval=1, commit_every=100)
+app = Application(
+    consumer_group=os.environ["CONSUMER_GROUP"], 
+    auto_offset_reset = "earliest",
+    commit_interval=1,
+    commit_every=100)
 
 input_topic = app.topic(os.environ["input"])
 

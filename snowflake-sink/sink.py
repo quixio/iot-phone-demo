b34b0c194753a7be955cd2f508993ed2352626fe
Tomas Neubauer <tomas@quix.io>
2024-12-05 17:22:54 +0000

    aaa

--- snowflake-sink/sink.py
+++ snowflake-sink/sink.py
@@ -1,61 +1,34 @@
-import json
-import logging
 import snowflake.connector
-from snowflake.connector import connect, DictCursor
-from quixstreams.sinks import BatchingSink
-from quixstreams.models import HeaderValue
-from typing import Any
+import logging
 
-class SnowflakeSink(BatchingSink):
+logger = logging.getLogger(__name__)
 
-    def __init__(self, account, user, password, warehouse, database, schema, table, role=None):
-        self.table = table
-        self.schema = schema
+class SnowflakeSink:
+    def __init__(self, warehouse, database, schema, table_name, account, user, password, logger):
+        self.warehouse = warehouse
         self.database = database
+        self.schema = schema
+        self.table_name = table_name
+        self.account = account
+        self.user = user
+        self.password = password
+        self.logger = logger
 
+    def connect(self):
         self.conn = snowflake.connector.connect(
-            user=user,
-            password=password,
-            account=account,
-            warehouse=warehouse,
-            database=database,
-            schema=schema,
-            role=role
+            user=self.user,
+            password=self.password,
+            account=self.account,
+            warehouse=self.warehouse,
+            database=self.database,
+            schema=self.schema
         )
+        self.cursor = self.conn.cursor()
 
-    def close(self):
-        self.conn.close()
-
-    def write(self, batch):
-        cursor = self.conn.cursor(DictCursor)
-
-        for item in batch:
-
-            cols = ",".join(item.value.keys())
-            vals = [json.dumps(v) if isinstance(v, dict) else v for v in item.value.values()]
-            vals = list(map(lambda x: str(x) if x is not None else 'NULL', vals))
-            sql = "INSERT INTO {} ({}) VALUES ({})".format(self.table, cols, ",".join(['%s'] * len(vals)))
-            cursor.execute(sql, tuple(vals))
-
-        self.conn.commit()
-        cursor.close()
-
-    def add(
-        self,
-        value: Any,
-        key: Any,
-        timestamp: int,
-        headers: list[tuple[str, HeaderValue]],
-        topic: str,
-        partition: int,
-        offset: int
-    ):
-        return super().add(
-            value=value,
-            key=key,
-            timestamp=timestamp,
-            headers=headers,
-            topic=topic,
-            partition=partition,
-            offset=offset
-        )
+    def write(self, data):
+        try:
+            self.cursor.executemany('INSERT INTO {} VALUES (%s, %s, %s)'.format(self.table_name), data)
+            self.conn.commit()
+        except Exception as e:
+            self.logger.error('Failed to insert data into snowflake: {}'.format(e))
+            self.conn.rollback()

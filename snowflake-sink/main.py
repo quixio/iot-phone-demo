dcc18bfc0628bebf020a04914baf0e61eb7eb999
Tomas Neubauer <tomas@quix.io>
2024-12-05 17:22:51 +0000

    aaa

--- snowflake-sink/main.py
+++ snowflake-sink/main.py
@@ -9,25 +9,31 @@ load_dotenv()
 
 from sink import SnowflakeSink
 
+TABLE_NAME = os.environ["TABLE_NAME"]
+WAREHOUSE = os.environ["WAREHOUSE"]
+DATABASE = os.environ["DATABASE"]
+SCHEMA = os.environ["SCHEMA"]
 ACCOUNT = os.environ["ACCOUNT"]
 USER = os.environ["USER"]
 PASSWORD = os.environ["PASSWORD"]
-WAREHOUSE = os.environ["WAREHOUSE"]
-DB = os.environ["DB"]
-SCHEMA = os.environ["SCHEMA"]
-TABLE = os.environ["TABLE"]
-ROLE = os.environ["ROLE"]
 
-snowflake_sink = SnowflakeSink(ACCOUNT, USER, PASSWORD, WAREHOUSE, DB, SCHEMA, TABLE, ROLE)
+snowflake_sink = SnowflakeSink(
+    WAREHOUSE, 
+    DATABASE, 
+    SCHEMA,
+    TABLE_NAME, 
+    ACCOUNT,
+    USER,
+    PASSWORD,
+    logger)
 
 snowflake_sink.connect()
 
 app = Application(
-    consumer_group=os.environ["CONSUMER_GROUP"],
-    auto_offset_reset="earliest",
+    consumer_group=os.environ["CONSUMER_GROUP"], 
+    auto_offset_reset = "earliest",
     commit_interval=1,
-    commit_every=100
-)
+    commit_every=100)
 
 input_topic = app.topic(os.environ["input"])
 

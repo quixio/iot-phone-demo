e19bc0782f718105d7f29cf1038d9dd7bd77ce79
Tomas Neubauer <tomas@quix.io>
2024-12-06 17:42:37 +0000

    aaa

--- snowflake-sink/main.py
+++ snowflake-sink/main.py
@@ -1,19 +1,33 @@
 import os
 from setup_logger import logger
-from dotenv import load_dotenv
-from quixstreams import Application
-from sink import SnowflakeSink
 
+from quixstreams import Application
+
+# for local dev, load env vars from a .env file
+from dotenv import load_dotenv
 load_dotenv()
 
-SNOWFLAKE_ACCOUNT = os.environ["SNOWFLAKE_ACCOUNT"]
-SNOWFLAKE_USER = os.environ["SNOWFLAKE_USER"]
-SNOWFLAKE_PASSWORD = os.environ["SNOWFLAKE_PASSWORD"]
+from sink import SnowflakeSink  # Replaced BigQuerySink with SnowflakeSink
+
+TABLE_NAME = os.environ["TABLE_NAME"]
 DATABASE = os.environ["DATABASE"]
 SCHEMA = os.environ["SCHEMA"]
 WAREHOUSE = os.environ["WAREHOUSE"]
+USER = os.environ["USER"]
+PASSWORD = os.environ["PASSWORD"]
+ACCOUNT = os.environ["ACCOUNT"]
 
-snowflake_sink = SnowflakeSink()
+snowflake_sink = SnowflakeSink(
+    ACCOUNT,
+    USER,
+    PASSWORD,
+    DATABASE,
+    SCHEMA,
+    WAREHOUSE,
+    TABLE_NAME,
+    logger)
+
+snowflake_sink.connect()
 
 app = Application(
     consumer_group=os.environ["CONSUMER_GROUP"], 
@@ -24,12 +38,7 @@ app = Application(
 input_topic = app.topic(os.environ["input"])
 
 sdf = app.dataframe(input_topic)
-
-def write_to_sink(sdf):
-    snowflake_sink.write(sdf)
-
-sdf.sink(write_to_sink)
+sdf.sink(snowflake_sink)
 
 if __name__ == "__main__":
     app.run(sdf)
-    snowflake_sink.close()

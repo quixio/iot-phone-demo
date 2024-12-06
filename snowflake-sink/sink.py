2e373714c3ec3013d123a75835a3051252dbe81f
Tomas Neubauer <tomas@quix.io>
2024-12-06 09:24:40 +0000

    aaa

--- snowflake-sink/sink.py
+++ snowflake-sink/sink.py
@@ -1,17 +1,20 @@
+import logging
 import snowflake.connector
 
-from quixstreams.sinks import Sink
+from quixstreams import Sink
 
+logger = logging.getLogger(__name__)
 
 class SnowflakeSink(Sink):
-    def __init__(self, warehouse, database, schema, table_name, account, user, password, logger):
-        self.warehouse = warehouse
-        self.database = database
-        self.schema = schema
-        self.table_name = table_name
+    def __init__(self, account, user, password, role, warehouse, database, schema, table, logger):
         self.account = account
         self.user = user
         self.password = password
+        self.role = role
+        self.warehouse = warehouse
+        self.database = database
+        self.schema = schema
+        self.table = table
         self.logger = logger
 
     def connect(self):
@@ -21,10 +24,15 @@ class SnowflakeSink(Sink):
             account=self.account,
             warehouse=self.warehouse,
             database=self.database,
-            schema=self.schema
+            schema=self.schema,
+            role=self.role,
         )
         self.cursor = self.conn.cursor()
 
     def write(self, batch):
         for item in batch:
-            self.cursor.execute(f"INSERT INTO {self.table_name} VALUES (%s)", (item.value,))
+            self.cursor.execute(
+                f"INSERT INTO {self.table} VALUES (%s, %s, %s)",
+                (item.key, item.value, item.timestamp)
+            )
+        self.conn.commit()

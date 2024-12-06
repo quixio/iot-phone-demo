c08b54a38d428b95a05442da8e0214186c653342
Tomas Neubauer <tomas@quix.io>
2024-12-06 10:52:04 +0000

    aaa

--- snowflake-sink/sink.py
+++ snowflake-sink/sink.py
@@ -4,7 +4,6 @@ import snowflake.connector
 from quixstreams import Application
 
 from quixstreams.exceptions import QuixException
-from quixstreams.models import HeadersTuples
 from quixstreams.sinks import BatchingSink, SinkBatch
 
 class SnowflakeSinkException(QuixException): ...

ed0e67a7dfc3f8118b0971adecb25ea107a24f30
Tomas Neubauer <tomas@quix.io>
2024-12-12 10:59:38 +0000

    Edited 'main.py'

--- anomaly-detection/main.py
+++ anomaly-detection/main.py
@@ -46,7 +46,9 @@ sdf = sdf[sdf["value"]["sum"] > 10]
 
 sdf = sdf.apply(lambda row: {
     "title": "Anomaly detected",
-    "message": f"Accelerometer {row['value']['device_id']} value changed by {row['value']['sum']} in last 5 seconds"
+    "message": f"Accelerometer {row['value']['device_id']} value changed by {row['value']['sum']} in last 5 seconds",
+    "device_id": row["device_id"],
+    "location": row["location"]
 })
 
 sdf.print()

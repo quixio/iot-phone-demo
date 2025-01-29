d196262db34843fc6f8ea0a4490ed41877a37d93
Tomas Neubauer <tomas@quix.io>
2025-01-29 20:28:20 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -14,6 +14,22 @@ sdf = app.dataframe(input_topic)
 
 def reduce_window(window:dict, row: dict):
 
+    for key, value in row.items():
+
+        if key == "timestamp":
+            continue
+        elif isinstance(value, (int, float)):
+            if key not in window:
+                window[key] = {
+                    "sum": value,
+                    "count": 1
+                }
+            else:
+                window[key]["sum"] += value
+                window[key]["count"] += 1
+        else:
+            window[key] = value
+
     
 
 sdf.print()

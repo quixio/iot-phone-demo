a0a02ec81bcc4bd7626c43c13873123a5ad6f79f
Tomas Neubauer <tomas@quix.io>
2025-01-29 20:37:59 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -6,20 +6,22 @@ from downsampling import reduce_window, init_window, aggregate_window
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest", use_changelog_topics=False)
+window_size_s = os.environ["window_size_s"]
+
+app = Application(consumer_group=f"downsampling-{window_size_s}-v1", auto_offset_reset="earliest", use_changelog_topics=False)
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-sdf = sdf.tumbling_window(int(os.environ["window_size_s"] / 1000), 5000) \
+sdf = sdf.tumbling_window(int(window_size_s) / 1000, 5000) \
     .reduce(reduce_window, init_window).final()
 
 sdf = sdf.apply(aggregate_window)
 
-sdf.print()
-#sdf.to_topic(output_topic)
+#sdf.print()
+sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run()

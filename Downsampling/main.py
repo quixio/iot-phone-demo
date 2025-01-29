8092c5e12c19c3c28edf5c6f40790ffcc25371ed
Tomas Neubauer <tomas@quix.io>
2025-01-29 20:35:43 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -12,43 +12,6 @@ output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-def reduce_window(window:dict, row: dict):
-
-    for key, value in row.items():
-
-        if key == "timestamp":
-            continue
-        elif isinstance(value, (int, float)):
-            if key not in window:
-                window[key] = {
-                    "sum": value,
-                    "count": 1
-                }
-            else:
-                window[key]["sum"] += value
-                window[key]["count"] += 1
-        else:
-            window[key] = value
-    
-    return window
-
-def init_window(row: dict):
-
-    window = {}
-    for key, value in row.items():
-
-        if key == "timestamp":
-            continue
-        elif isinstance(value, (int, float)):
-            window[key] = {
-                "sum": value,
-                "count": 1
-            }
-        else:
-            window[key] = value
-
-    return window
-
 sdf = sdf.tumbling_window(5000, 5000).reduce(reduce_window, init_window).final()
 
 def aggregate_window(window: dict):

e4905108deb00ee54be47575b1a5f66b890ee861
Tomas Neubauer <tomas@quix.io>
2025-01-29 20:35:54 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -14,19 +14,7 @@ sdf = app.dataframe(input_topic)
 
 sdf = sdf.tumbling_window(5000, 5000).reduce(reduce_window, init_window).final()
 
-def aggregate_window(window: dict):
-    result = {
-        "timestamp": window["end"] * 1E6
-    }
 
-    for key, value in window["value"].items():
-
-        if "sum" in value:
-            result[key] = value["sum"] / value["count"]
-        else:
-            result[key] = value
-
-    return result
 
 
 sdf = sdf.apply(aggregate_window)

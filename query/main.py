075313b19790fb4d7cf9ae238ee9806ce9d17681
Tomas Neubauer <tomas@quix.io>
2025-02-06 11:18:54 +0000

    Edited 'main.py'

--- query/main.py
+++ query/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 from flasgger import Swagger
+import json
 
 app = Flask(__name__)
 
@@ -26,7 +27,7 @@ def get_documents():
     for doc in documents:
         print(doc)
 
-    return jsonify(documents)
+    return json.dumps(documents)
 
 @app.route("/documents", methods=["POST"])
 def add_document():

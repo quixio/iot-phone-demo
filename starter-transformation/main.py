04c4124b48646ad3370384ee66db4e97e3594896
Tomas Neubauer <tomas@quix.io>
2025-02-07 11:20:34 +0000

    Edited 'main.py'

--- starter-transformation/main.py
+++ starter-transformation/main.py
@@ -16,7 +16,7 @@ sdf = app.dataframe(input_topic)
 # see docs for what you can do
 # https://quix.io/docs/get-started/quixtour/process-threshold.html
 #sdf.print()
-sdf.print_live_table(number=5, slowdown=0.5, title="Live Records")
+sdf.print_table(number=5, slowdown=0.5, title="Live Records")
 #sdf.to_topic(output_topic)
 
 if __name__ == "__main__":

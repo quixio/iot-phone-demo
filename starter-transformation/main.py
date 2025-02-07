d7d818d56040fea19ce62834422c5571ddd96608
Tomas Neubauer <tomas@quix.io>
2025-02-07 11:42:18 +0000

    Edited 'main.py'

--- starter-transformation/main.py
+++ starter-transformation/main.py
@@ -5,7 +5,7 @@ from quixstreams import Application
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1.1", auto_offset_reset="earliest")
+app = Application(consumer_group="transformation-v1.2", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])

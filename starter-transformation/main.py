b238432ed700a73afed2ff5c3ffb0c14c97df16f
Tomas Neubauer <tomas@quix.io>
2025-02-07 11:19:21 +0000

    Edited 'main.py'

--- starter-transformation/main.py
+++ starter-transformation/main.py
@@ -15,7 +15,7 @@ sdf = app.dataframe(input_topic)
 # put transformation logic here
 # see docs for what you can do
 # https://quix.io/docs/get-started/quixtour/process-threshold.html
-
+sdf.print()
 sdf.print_table()
 #sdf.to_topic(output_topic)
 

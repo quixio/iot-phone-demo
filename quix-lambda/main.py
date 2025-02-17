1268500e7782fb43d0856c8460828893b0efd54a
Tomas Neubauer <tomas@quix.io>
2025-02-17 12:59:28 +0000

    Edited 'main.py'

--- quix-lambda/main.py
+++ quix-lambda/main.py
@@ -8,16 +8,14 @@ load_dotenv()
 app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+#output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+sdf = sdf.tumbling_window(10000, 5000).collect().final()
 
 sdf.print()
-sdf.to_topic(output_topic)
+#sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run()

3de7c9f56932fd0754af3307d0edb444ae40bdc9
Tomas New Bauer <tomas+staging@quix.io>
2025-08-20 13:15:10 +0000

    Edited 'main.py'

--- raw-to-table/main.py
+++ raw-to-table/main.py
@@ -30,7 +30,7 @@ def expand_values_to_columns(message: dict):
 
 sdf = sdf.apply(expand_values_to_columns, expand=True)
 
-sdf = sdf.hopping_window(5000, 250).reduce(lambda state, row: { **state, **row}, lambda row: row).final()
+sdf = sdf.hopping_window(5000, 50).reduce(lambda state, row: { **state, **row}, lambda row: row).final()
 
 sdf = sdf.apply(lambda row:{
     "timestamp": row["start"],
